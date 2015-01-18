403b55200b814c341e54a27f5c2f1ca696ae8d1e
morose <morose@localhost.localdomain>
2015-01-18 15:40:51 +0300

    Overwrite when inserting

--- .emacs.d/ui.el
+++ .emacs.d/ui.el
@@ -39,7 +39,7 @@
 (setq ;; makes killing/yanking interact with the clipboard
  x-select-enable-clipboard t
  ;; I'm actually not sure what this does but it's recommended?
- x-select-enable-primary t
+ ;;x-select-enable-primary t
  ;; Save clipboard strings into kill ring before replacing them.
  ;; When one selects something in another program to paste it into Emacs,
  ;; but kills something in Emacs before actually pasting it,

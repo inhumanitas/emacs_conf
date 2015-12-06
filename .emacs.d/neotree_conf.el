33954ec0bd3ae026622830fd4d261ad6522c0915
morose <morose@morose.notebook>
2015-12-06 15:18:45 +0300

    Added neotree

---
+++ .emacs.d/neotree_conf.el
@@ -0,0 +1,9 @@
+;; NEO tree
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+(global-set-key (kbd "M-n f") 'neotree-find)
+(global-set-key (kbd "M-n s") 'neotree-show)
+(global-set-key (kbd "M-n F") 'neotree-change-root)
+(global-set-key [M-up] 'neotree-select-up-node)
+(global-set-key [M-down] 'neotree-select-down-node)
+(global-set-key [C-f8] 'neotree-stretch-toggle)
